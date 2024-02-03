bacaf9415d30b1c1c51c999068e346ba64fdc6d1
JuanH941214 <juanhernandez941214@gmail.com>
2024-02-03 17:51:36 +0100

    first commit

---
+++ database/migrations/2024_02_03_160632_user_match.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->integer('diceOne');
+        $table->integer('diceTwo');
+        $table->integer('sum');
+        $table->unsignedBigInteger('user_id');
+        $table->unsignedBigInteger('match_id');
+        $table->timestamps();
+
+        $table->foreign('usuario_id')->references('id')->on('create_users_table');
+        $table->foreign('match_id')->references('id')->on('match');
+        
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
